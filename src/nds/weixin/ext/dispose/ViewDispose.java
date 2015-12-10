b4e19d58dc89ec0377a1a37bdf9b247243d8e745
cuijiwen <cjwfup@163.com>
2015-12-10 19:57:05 +0800

    qwert

--- src/nds/weixin/ext/dispose/ViewDispose.java
+++ src/nds/weixin/ext/dispose/ViewDispose.java
@@ -15,7 +15,7 @@ public class ViewDispose implements IMessageDispose {
 	@Override
 	public void dispose(HttpServletRequest request,HttpServletResponse response, WxPublicControl wpc, JSONObject jo) {
 		logger.debug("viewDispose->");
-		logger.debug("test git-hub");
+		logger.debug("test git-hub 32121342134");
 	}
 
 }
