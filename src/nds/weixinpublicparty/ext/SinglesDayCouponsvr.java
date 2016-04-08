86fee9e0cd10e8fcbe57ba5455b8a9fd960ebbc7
JavaCodeCui <824760983@qq.com>
2016-04-08 12:48:58 +0800

    发券相关

--- src/nds/weixinpublicparty/ext/SinglesDayCouponsvr.java
+++ src/nds/weixinpublicparty/ext/SinglesDayCouponsvr.java
@@ -5,6 +5,7 @@ import nds.control.util.ValueHolder;
 import nds.control.web.ClientControllerWebImpl;
 import nds.control.web.WebUtils;
 import nds.process.SvrProcess;
+import nds.weixinpublicparty.ext.common.CommonSendCoupon;
 
 public class SinglesDayCouponsvr extends SvrProcess {
 
@@ -18,7 +19,22 @@ public class SinglesDayCouponsvr extends SvrProcess {
 	protected String doIt() throws Exception {
 		// TODO Auto-generated method stub
 		ValueHolder vh=null;
-		try{
+		String couponsql="select fc.ad_client_id,fc.id,cp.num,nvl(cp.usetype1,2),nvl(cp.value,'0'),to_char(nvl(cp.starttime,sysdate), 'YYYYMMDD'),to_char(decode(nvl(cp.validay,0),0, nvl(cp.endtime, add_months(sysdate,1)),nvl(cp.starttime,sysdate)+nvl(cp.validay,0)), 'YYYYMMDD')"
+				+",get_fitler_sql(fc.releasevips) from wx_festivalcoupon fc join WX_COUPON cp on cp.id=fc.issuingcoupons"
+				+" where nvl(cp.isactive, 'N') = 'Y'"
+				+" and fc.releasetype = 'A'"
+				+" and nvl(fc.status,'1') = '2'"
+				+" and nvl(fc.whethernot,'Y') = 'N'"
+				+" and to_char(sysdate,'YYYYMMDD')<to_char(decode(nvl(cp.validay,0),0, nvl(cp.endtime, add_months(sysdate,1)),nvl(cp.starttime,sysdate)+nvl(cp.validay,0)), 'YYYYMMDD')";
+    //查出要发的会员
+	String vipsql="select v.id,v.wechatno,v.vipcardno"
+		   +" from   wx_vip v"
+		   +" where  v.ad_client_id=?"
+		   +" and v.id ";
+	//更新是否发放
+	String updatesql="update wx_festivalcoupon fc set fc.whethernot='Y' where fc.id=?";
+		vh = CommonSendCoupon.sendCoupon(couponsql,vipsql,updatesql,log);
+/*		try{
 			ClientControllerWebImpl controller=(ClientControllerWebImpl)WebUtils.getServletContextManager().getActor(nds.util.WebKeys.WEB_CONTROLLER);
 			DefaultWebEvent event=new DefaultWebEvent("CommandEvent");
 
@@ -27,7 +43,7 @@ public class SinglesDayCouponsvr extends SvrProcess {
 		}catch(Exception e){
 			log.debug("call nds.weixinpublicparty.ext.SinglesDayCommand error->"+e.getLocalizedMessage());
 			e.printStackTrace();
-		}
+		}*/
 		return null;
 	}
 
